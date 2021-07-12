36bee05e249920d097348697c51434480994dfa4
futasio <71268955+futasio@users.noreply.github.com>
2021-07-12 22:23:39 +0900

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -10,20 +10,8 @@ document.addEventListener("scroll", function(){
             targetElement[i].classList.add("show");
         }
     }
-    
 });
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 0) {
-        $('header').css('background-color', '#EDEFE1');
-        $('header').css('height', '80px');
-      } else {
-        $('header').css('background-color', '#ababab');
-        $('header').css('height', '110px');
-      }
-    });
-  });
 
   $(function(){
     $('a[href^=#]').click(function(){
@@ -34,5 +22,17 @@ $(document).ready(function() {
       var position = target.offset().top + adjust;
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
+    });
+  });
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 0) {
+        $('header').css('background-color', '#EDEFE1');
+        $('header').css('height', '80px');
+      } else {
+        $('header').css('background-color', '#ababab');
+        $('header').css('height', '110px');
+      }
     });
   });
